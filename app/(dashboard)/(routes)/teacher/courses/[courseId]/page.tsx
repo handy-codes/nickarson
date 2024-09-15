fae46d5f3bb87aff00f0da02554949c70b529647
emeka <emekaowo@yahoo.com>
2024-09-15 15:15:34 +0100

    complete confetti implementation

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -11,9 +11,8 @@ import CategoryForm from "./_components/category-form";
 import PriceForm from "./_components/price-form";
 import { AttachmentForm } from "./_components/attachment-form";
 import ChaptersForm from "./_components/chapters-form";
-// import { Banner } from "@/components/banner";
-// import { ChaptersForm } from "./_components/chapters-form";
-// import { Actions } from "./_components/actions";
+import { Banner } from "@/components/banner";
+import { Actions } from "./_components/actions";
 
 const CourseIdPage = async ({
   params
@@ -70,34 +69,15 @@ const CourseIdPage = async ({
     const completedFields = requiredFields.filter(Boolean).length;
 
     const completionText = `(${completedFields}/${totalFields})`
-
-
-
-
-
-//   const requiredFields = [
-//     course.title,
-//     course.description,
-//     course.imageUrl,
-//     course.price,
-//     course.categoryId,
-//     course.chapters.some(chapter => chapter.isPublished),
-//   ];
-
-//   const totalFields = requiredFields.length;
-//   const completedFields = requiredFields.filter(Boolean).length;
-
-//   const completionText = `(${completedFields} / ${totalFields})`;
-
-//   const isComplete = requiredFields.every(Boolean);
+    const isComplete = requiredFields.every(Boolean);
 
   return (
     <>
-      {/* {!course.isPublished && (
+      {!course.isPublished && (
         <Banner
           label="This course is unpublished. It will not be visible to the students."
         />
-      )} */}
+      )}
       <div className="p-6">
         <div className="flex items-center justify-between">
           <div className="flex flex-col gap-y-2">
@@ -108,11 +88,11 @@ const CourseIdPage = async ({
               Complete all fields {completionText}
             </span>
           </div>
-          {/* <Actions
+          <Actions
             disabled={!isComplete}
             courseId={params.courseId}
             isPublished={course.isPublished}
-          /> */}
+          />
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
           <div>
